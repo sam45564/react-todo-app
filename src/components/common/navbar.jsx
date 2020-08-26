07ad4b5054bd301d6fce2e6d3120722b7d96dc1c
Samsul Hoque Choudhury <sam45564@gmail.com>
2020-08-26 11:21:03 +0530

    Added main app component and composed navbar, dashboard component into it.

---
+++ src/components/common/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav class="navbar navbar-dark bg-info">
+            <span class="navbar-brand mb-0 h1">My Todo</span>
+        </nav>
+    );
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from "./components/app";
 import * as serviceWorker from './serviceWorker';
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(
   <React.StrictMode>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
